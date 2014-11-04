6f488390c933f52d570d12b887622b919695478d
linme882@student.liu.se <linme882@student.liu.se>
2014-11-04 10:24:35 +0100

    added the skeleton for the program

---
+++ tnm034.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+function strout = tnm034(Im)
+%
+% Im: Input image of the captured QR-code. Im should be in 
+% double format, normalized to the interval [0,1]
+%
+% strout: The resulting character string of the coded message. 
+% The string must follow the pre-defined format, explained below.
+%
+% Our program code...
+strout=char(string); 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
