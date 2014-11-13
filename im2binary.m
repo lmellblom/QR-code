8be2bd0db7dba52a339c10bab32e2fcadabf8ae6
Jonie90 <white-sorrow@hotmail.com>
2014-11-13 12:27:24 +0100

    Added information text

--- im2binary.m
+++ im2binary.m
@@ -1,3 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%function bin = im2binary(img)
+%
+%Computes a global threshold using graythresh() and uses this threshold to convert 
+%the image to binary with im2bw().
+%Part of a pattern recognition project TNM034 - Advanced Image Processing, Linköping
+%University HT2014.
+%
+%Copyright (c) <2014> Karolin Jonsson, Louise Carlström, Linnea Nåbo, Linnea Mellblom
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function bin = im2binary(img)
 level = graythresh(img);
 BW = im2bw(img,level);
