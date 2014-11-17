015b8eccc416d26f72c896e6a6a61dc522123dac
linme882@student.liu.se <linme882@student.liu.se>
2014-11-17 15:46:06 +0100

    finding the FIPs almost works. little ugly sometimes, but still

--- testDecodeQr.m
+++ testDecodeQr.m
@@ -4,10 +4,11 @@ close all;
 
 % just a test image at the moment, read in the "real" image later
 img = imread('images/img_set2/test','png');    %testimage that i cropped
-
+imshow(img)
 % process the image and all stuff
-testImg = double(im2bw(img, graythresh(img))); % makes a binary image depending on grayvalue
+testImg = double(im2binary(img)); % makes a binary image depending on grayvalue
 % im2bw gives 0 or 1 depending on the threshold value of img. 
+% double to convert so not logical array. 
 
 % get the message from the now image that is 41x41 pixels and ready to read
 msg = decodeQR(testImg);
