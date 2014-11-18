d3455515053737f0a43b7691bdee494e3bb6f2d5
NaiLi <linnea.nabo@gmail.com>
2014-11-18 12:44:00 +0100

    some changes in the transform and a ffunction to go from big im to 41x41

--- perspectiveTransform.m
+++ perspectiveTransform.m
@@ -17,7 +17,7 @@ function transformed = perspectiveTransform(Im, col_values, row_values)
 
 % Make the new image the same size as the current (not necessary the best
 % option)
-Im = rgb2gray(Im);
+Im = rgb2gray(Im);  %problem when im is allready blw
 [width,height] = size(Im);
 
 % Calculate true values
@@ -26,25 +26,27 @@ dist_1 = abs((row_values(1) - row_values(2)));
 dist_2 = abs((col_values(3) - col_values(2)));
 
 dist_max = max(dist_1, dist_2);
-dist_min = dist_max*0.09756;
+dist_min = dist_max*(3.5/34);
+side = dist_max + (2*dist_min);
 
-col_values_true = [dist_min;          dist_min; dist_min+dist_max; dist_max+dist_min];
-row_values_true = [dist_max+dist_min; dist_min; dist_min         ; dist_max+dist_min];
+col_values_true = [dist_min;          dist_min; dist_min+dist_max];%; dist_max+dist_min];
+row_values_true = [dist_max+dist_min; dist_min; dist_min         ];%; dist_max+dist_min];
 
 
 % Select the points in the image and the reference image
-number_of_points = 4;
+number_of_points = 3;
 
 % Collect coordinates from the image and the reference image
-false_pic = [row_values(:) col_values(:) ones(number_of_points,1)] %values of the false pic
-true_pic = [row_values_true(:) col_values_true(:) ones(number_of_points,1)] %values of the true pic
+false_pic = [row_values(:) col_values(:) ones(number_of_points,1)]; %values of the false pic
+true_pic = [row_values_true(:) col_values_true(:) ones(number_of_points,1)]; %values of the true pic
 
 % Find transformation matrix
 x = false_pic\true_pic;
 
 % Fit geometry
-[X,Y] = meshgrid(1:height,1:width);
-M = [Y(:) X(:) ones(width*height,1)];
+[X,Y] = meshgrid(1:height,1:width);%[X,Y] = meshgrid(1:dist_max,1:dist_max);%
+M = [Y(:) X(:) ones(width*height,1)];%M = [Y(:) X(:) ones(dist_max*dist_max,1)];
+
 
 % Translate and get matrix P
 P = M(:,1:3)*x;
@@ -56,25 +58,17 @@ for k=1:(width*height)
         v = P(k,2);
         w = P(k,3);
         
-    if(0<u && u<=width && 0<v && v<=height)          
+    if(0<u && u<=side && 0<v && v<=side)          
         newim(u/w,v/w) = Im(k);   
     end
     
 end
 
 newim = im2binary(newim);
-se = strel('square',20); %There is probably a better option
+n = floor(dist_max/(33*2));
+se = strel('square',n); %There is probably a better option, need to decide what square side
 newim = imclose(newim,se);
 figure;
 imshow(newim);
 
 transformed = newim;
-
-
-% Manually chosen points (should come from detection)
-% for: imread('images/img_set5/Hus_4.png');
-%col_values = [1148; 1320; 2246; 2050; 1090; 1278; 2076; 1986]; %outer, inner of f1,2,3,4
-%row_values = [376;  560;  384;   568; 1456; 1284; 1376; 1296];
-%col_values_true = [0; 70; 410; 340; 0;   60;  370; 320];
-%row_values_true = [0; 70; 0;   70;  410; 340; 370; 320];
-
