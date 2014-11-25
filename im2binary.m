6c1d41a815506f5683a09ca790997502b4a16aba
louca859 <louca859@student.liu.se>
2014-11-25 14:05:11 +0100

    Rewrote im2binary so that it now uses adaptive thresholding. When calling the function in findFIP.m the number 3 (number of thresholds) is now sent as an argument. This number can of course be changed

--- im2binary.m
+++ im2binary.m
@@ -11,7 +11,32 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function bin = im2binary(img)
-level = graythresh(img);
-BW = im2bw(img,level);
-bin = BW;
+function bin = im2binary(img, k)
+
+imshow(img);
+figure;
+
+%Skapa en matris där k*k st tröskelvärden sparas
+thresholds = zeros(k);
+
+[height, width, depth] = size(img); % height and width (depth is not used but needs to be there so "size" works correctly
+
+BW_img = zeros(height, width);
+
+h_step = floor(height/k);
+w_step = floor(width/k);
+
+place = 1;
+for i=1:h_step:height
+    for j=1:w_step:width
+        if(place <= k*k)
+            thresholds(place) = graythresh( img((i:i+(h_step-1)), (j:j+(w_step-1))) );
+            BW_img( (i:(i+(h_step-1))), (j:(j+(w_step-1))) ) = im2bw( (img( (i:i+(h_step-1)), (j:j+(w_step-1)) )), thresholds(place) );
+            place = place+1;
+        end
+    end
+end
+
+imshow(BW_img);
+
+bin = BW_img;
