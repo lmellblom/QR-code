f4e37bb03df2d6a0f4eeca3637b0e1bd08ec91e1
louca859 <louca859@student.liu.se>
2014-11-27 11:46:36 +0100

    adaptive thresholding fixed, now with 8x8 thresholds

--- adaptive_threshold.m
+++ adaptive_threshold.m
@@ -15,36 +15,28 @@ function thresholds = adaptive_threshold(img, k)
 
 %img = imread('images/img_newSet/Husannons_full_Illum.png');
 img = imread('images/img_set2/Hus_1.png');
-k = 3;
+k = 8;
 
 imshow(img);
 figure;
 
-%Skapa en matris där k*k st tröskelvärden sparas
-thresholds = zeros(k)
-
-[height, width, depth] = size(img) % height and width (depth is not used but needs to be there so "size" works correctly
+[height, width, depth] = size(img); % height and width (depth is not used but needs to be there so "size" works correctly
 
 BW_img = zeros(height, width);
-size_BW = size(BW_img)
 
 h_step = floor(height/k)
 w_step = floor(width/k)
 
 place = 1;
-for i=1:h_step:height
-    for j=1:w_step:width
+for i=1:h_step:(height-h_step)
+    for j=1:w_step:(width-w_step)
         if(place <= k*k)
-            place
-            thresholds(place) = graythresh( img((i:i+(h_step-1)), (j:j+(w_step-1))) );
-            BW_img( (i:(i+(h_step-1))), (j:(j+(w_step-1))) ) = im2bw( (img( (i:i+(h_step-1)), (j:j+(w_step-1)) )), thresholds(place) );
+            temp_tresh = graythresh( img((i:i+h_step-1), (j:j+w_step-1)) )
+            BW_img( (i:i+h_step-1), (j:j+w_step-1) ) = im2bw( (img( (i:i+h_step-1), (j:j+w_step-1) )), temp_tresh );
             place = place+1;
         end
     end
 end
-
-size_BW = size(BW_img)
-size_img = size(img)
 
 imshow(BW_img);
 
