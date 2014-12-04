1af4895e567973a394256185a20b7d9c15b2edcd
NaiLi <linnea.nabo@gmail.com>
2014-12-04 12:23:26 +0100

    work on the adaptive im2binaryFix

--- im2binaryFix.m
+++ im2binaryFix.m
@@ -16,14 +16,12 @@ function thresholds = im2binaryFix(img, k)
 %% Test param. remove when working
 k = 8;
 %img = imread('images/img_set2/test.png');
-img = imread('images/img_set1/Bygg_1.png');
+%img = imread('images/img_set1/Bygg_1.png');
 %img = imread('images/img_newSet/Husannons_full_Illum.png');
 
-uniqLevels = unique(img(:));
-disp(['Number of unique levels = ' int2str( length(uniqLevels) )]);
+uniqueLevels = unique(img(:)); %Används aldrig
+%disp(['Number of unique levels = ' int2str( length(uniqLevels) )]);
 
-figure(1);
-imshow(img);
 %%
 %Skapa en matris där k*k st tröskelvärden sparas
 thresholds = zeros(k);
@@ -35,10 +33,8 @@ if(depth == 3)
 end
 
 img = imadjust(img);
-figure(7);imshow(img);
 
-uniqLevels = unique(img(:));
-disp(['Number of unique levels = ' int2str( length(uniqLevels) )]);
+uniqLevels = unique(img(:)); % Används aldrig
 
 %% Calculate padding
 % If the remainder of height / k is 0, k pixels will used for padding!!
@@ -61,9 +57,6 @@ img = [img, padCol];
 %Add row padding at the bottom of the image
 img = [img; padRow]; %REMEMBER TO DELETE THE PADDING AFTER TRESHOLDING
 
-figure(2);
-imshow(img);
-
 % _step: the step with which to loop to threshold
 h_step = (paddedHeight/k);
 w_step = (paddedWidth/k);
@@ -74,45 +67,37 @@ loopWidth = paddedWidth;
 %% Create new empty image
 BW_img = zeros(size(img));
 
-figure(3);
-imshow(BW_img);
-
 %% det blir olika bokstäver när man decodar beroende på om man tex trösklar med k=8 eller k=9, skulle man kunde ta det som blev rätt från båda så skulle det bli perfa. 
 
 place = 1;
 entire_thresh = graythresh(img);
+img = im2double(img);
 
 for i=1:h_step:(loopHeight-h_step+1)
     for j=1:w_step:(loopWidth-w_step+1)
         temp_img = img( (i:i+(h_step-1)), (j:j+(w_step-1)) );
         
-        temp_mean = mean(temp_img);
+        small_thresh = graythresh(temp_img);
         
-        temp_min = min(min(temp_img))
-        temp_max = max(max(temp_img))
-        delta = temp_max - temp_min
+        temp_min = min(min(temp_img));
+        temp_max = max(max(temp_img));
+        delta = temp_max - temp_min;
         
-        if( place <= k*k && delta > 40 )
-            [level, EM] = graythresh( temp_img );
-            thresholds(place) = level;
+        if( place <= k*k && delta > 0.946 )%0.7 för nästan alla är bra (nästan skjuter ingen hare)
+            %[level, EM] = graythresh( temp_img );
+            thresholds(place) = small_thresh;%level;
             BW_img( (i:(i+(h_step-1))), (j:(j+(w_step-1))) ) = im2bw( (img( (i:i+(h_step-1)), (j:j+(w_step-1)) )), thresholds(place) );
             place = place+1;
-        elseif(entire_thresh > level)
-            BW_img( (i:(i+(h_step-1))), (j:(j+(w_step-1))) ) = 1;
-        else
+        elseif(entire_thresh > small_thresh)
             BW_img( (i:(i+(h_step-1))), (j:(j+(w_step-1))) ) = 0;
+        else
+            BW_img( (i:(i+(h_step-1))), (j:(j+(w_step-1))) ) = 1;
         end
     end
 end
 
-figure(4);
-imshow(BW_img);
-
 %% Remove padding
 BW_img = BW_img(1:height, 1:width);
 
-figure(5);
-imshow(BW_img);
+thresholds = BW_img;
 
-%%
-bin = BW_img;
