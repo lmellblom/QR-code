1ce097442dfd7badc37f40669062938bf44c648a
NaiLi <linnea.nabo@gmail.com>
2014-12-10 12:33:10 +0100

    new and improved perspective transformation

--- perspectiveTransform.m
+++ perspectiveTransform.m
@@ -13,32 +13,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function transformed = perspectiveTransform(Im, fips, ap)
 
-no_of_points = 4;
-% Set the fips
-row_values_dist = [fips(1,1), fips(2,1), fips(3,1), ap(1)];
-col_values_dist = [fips(1,2), fips(2,2), fips(3,2), ap(2)];
-
-%figure;
-%imshow(Im);
-%hold on;
-%plot(fips(1,2), fips(1,1),'g*');
-%plot(fips(2,2), fips(2,1),'r*');
-%plot(fips(3,2), fips(3,1),'b*');
-%plot(ap(2), ap(1),'c*');
-
-% Make the new image the same size as the current
-% [width,height,color] = size(Im);
-% if(color == 3)
-%     Im = rgb2gray(Im);
-% end
-[width,height] = size(Im);
+% The fip points of the distorted image 
+moving_points = [fips(1,2) fips(1,1);
+                    fips(2,2) fips(2,1);
+                        fips(3,2) fips(3,1);
+                            ap(2) ap(1)];
 
     % A-------C
     % |    _/
     % |  _/
     % |_/
     % B/
-    
+
+% Receive the vectors between the fips
 A = fips(2,:);
 B = fips(1,:);
 C = fips(3,:);
@@ -46,69 +33,38 @@ C = fips(3,:);
 AC = C-A;
 AB = B-A;
     
-% Calculate true values
-% Find max distance:
+% Find distance between the fips
 dist_1 = sqrt(AC(1)^2 + AC(2)^2);
 dist_2 = sqrt(AB(1)^2 + AB(2)^2); 
 
+% Find the correct values for the transformed fip
+% the length between the fips is based on the longest distance
+% between the fips in the distorted image
 dist_max = max(dist_1, dist_2);
 cell_width = dist_max/34;
-dist_min = 3.5*cell_width;%dist_max*(3.5/36);%(3.5/34)%
-side = 41*cell_width;%dist_max + (2*dist_min);
+side = 41*cell_width;
+dist_min = 3.5*cell_width;
 dist_fip = 37.5*cell_width;
 dist_ap = 34.5*cell_width;
 
+% The correct coordinates for the QR code
+fixed_points = [dist_min dist_fip;
+                    dist_min dist_min;
+                        dist_fip dist_min;
+                            dist_ap dist_ap];
 
-% True values for the fips
-col_values_true = [dist_min;          dist_min; dist_fip;          dist_ap];
-row_values_true = [dist_fip;          dist_min; dist_min         ; dist_ap];
-% plot(row_values_true(1), col_values_true(1),'y*');
-% plot(row_values_true(2), col_values_true(2),'y*');
-% plot(row_values_true(3), col_values_true(3),'y*');
-% plot(row_values_true(4), col_values_true(4),'y*');
+% Find and perform the transformation for the image                        
+tform = fitgeotrans(moving_points, fixed_points, 'projective');
+newim = imwarp(Im, tform, 'linear');
 
+% Make the transformed image binary and find the fips again
+newim = im2binarySimple(newim);
+FIPCandidates = findFIPCandidates(newim);
+FIPLocations = findFIPs(FIPCandidates);
 
-% Collect coordinates from the image and the reference image
-false_pic = [row_values_dist(:) col_values_dist(:) ones(no_of_points,1)]; %values of the false pic
-true_pic = [row_values_true(:) col_values_true(:) ones(no_of_points,1)]; %values of the true pic
-
-% Find transformation matrix
-x = false_pic\true_pic;
-
-% Fit geometry
-[X,Y] = meshgrid(1:height,1:width);%[X,Y] = meshgrid(1:dist_max,1:dist_max);%
-M = [Y(:) X(:) ones(width*height,1)];%M = [Y(:) X(:) ones(dist_max*dist_max,1)];
-
-
-% Translate and get matrix P
-P = M(:,1:3)*x;
-P = round(P);
-
-% Loop through every pair of coordiantes, check if positive
-for k=1:(width*height)
-        u = P(k,1);
-        v = P(k,2);
-        w = P(k,3);
-        
-    if(0<u && u<=side && 0<v && v<=side)          
-        newim(u/w,v/w) = Im(k);   
-    end
-    
-end
-
-n = floor(cell_width/2);
-if(cell_width > 2)
-    se = strel('square',n); %There is probably a better option, need to decide what square side
-    newim = imclose(newim,se);
-end
-
-% figure;
-% imshow(newim);
-% hold on;
-% plot(row_values_true(1), col_values_true(1),'y*');
-% plot(row_values_true(2), col_values_true(2),'y*');
-% plot(row_values_true(3), col_values_true(3),'y*');
-% plot(row_values_true(4), col_values_true(4),'y*');
+% Crop the image from the top left corner to a rectangle
+% with the same size as the QR code
+rect = [FIPLocations(2,2)-(3.5*cell_width) FIPLocations(2,1)-(3.5*cell_width) side side];
+newim = imcrop(newim, rect);
 
 transformed = newim;
-
