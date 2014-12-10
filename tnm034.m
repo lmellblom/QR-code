135ca08b8ba695658fb290125937cd1f5d1281c2
NaiLi <linnea.nabo@gmail.com>
2014-12-10 12:45:49 +0100

    making it beautiful!

--- tnm034.m
+++ tnm034.m
@@ -1,5 +1,6 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-function strout = tnm034(Im)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%function strout = tnm034(Im)
 %
 % Im: Input image of the captured QR-code. Im should be in 
 % double format, normalized to the interval [0,1]
@@ -7,37 +8,28 @@ function strout = tnm034(Im)
 % strout: The resulting character string of the coded message. 
 % The string must follow the pre-defined format, explained below.
 %
-% Our program code...
+% Copyright (c) <2014> Karolin Jonsson, Louise Carlström, Linnea Nåbo, Linnea Mellblom
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function strout = tnm034(Im)
 
-% process the image, maybe check if gray image or something first??
-%img = im2binary(Im, 4);
+% Make image binary
+img = im2binarySimple(img);
 
-img = im2binarySimple(Im);
-
-% find FIPs
+% find FIP points in the image
 FIPCandidates = findFIPCandidates(img);
 FIPLocations = findFIPs(FIPCandidates);
 
-% rotate the pic
-%img = rotationTransform(img, FIPLocations);
-
-% find FIPs
-%FIPCandidates = findFIPCandidates(img);
-%FIPLocations = findFIPs(FIPCandidates);
-
-% find AP
+% find AP point in the image
 APLocation = findAP(FIPLocations,img); 
 
-% transform the pic
-img = perspectiveTransform(img, FIPLocations, APLocation);
-%figure;
-%imshow(img);
+% Transform the image
+img = perspectiveTransform(Im, FIPLocations, APLocation);
 
+% Resize image
 small_im = imresize(img, [41 41], 'nearest');
-%figure;
-%imshow(small_im);
 
-% read the QR-code
+% Read the QR-code
 message = decodeQR(double(small_im));
     
-strout=message;%char(message); 
+strout=message;
